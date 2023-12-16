00f9b70686d81cdcedf457b715ee692191d5d54e
Akshmit <saxena.akshmit@gmail.com>
2023-12-16 11:24:31 +0530

    fonts and splash screen added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Inter-ExtraBold', fontSize: 30 }}>
+        Inter-ExtraBold
+      </Text>
+      <Text style={{ fontFamily: 'Inter-Medium', fontSize: 20 }}>
+      Inter-Medium
+      </Text>
+      <Text style={{ fontFamily: 'Inter-Regular', fontSize: 10 }}>
+        Inter-Regular
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
